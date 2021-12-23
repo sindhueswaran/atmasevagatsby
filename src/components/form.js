d84535f8686d2dde89688ccf579d0c37054579cc
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2021-12-23 21:24:20 +0530

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -9,7 +9,7 @@ const Form = () => {
       <h2 className="text-title"> Do get in touch </h2>
         <form className="d-grid" name="contact" method="POST" action="/thankyou" data-netlify="true" netlify-honeypot="bot-field">
         <p class="hidden">
-          <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+          <label>Don’t fill this out if you’re human: <input name="bot-field" type="hidden" name="form-name" value="contact" /></label>
         </p>
               <div className="mb-3">
                 <input type="text" name="name" className="form-control text-center rounded-0" placeholder="Your Name"/> 
