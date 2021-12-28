0935038a0385033dd890939da76489eb68bdc5bc
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2021-12-28 11:55:26 +0530

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -10,13 +10,13 @@ const Form = () => {
         <form className="d-grid" name="contact" method="POST" action="/thankyou" data-netlify="true" netlify-honeypot="bot-field">
           <input name="bot-field" type="hidden" name="form-name" value="contact" />
               <div className="mb-3">
-                <input type="text" name="name" className="form-control text-center rounded-0" placeholder="Your Name"/> 
+                <input type="text" name="name" className="form-control text-center rounded-0" placeholder="Your Name" required/> 
               </div>
               <div className="mb-3"> 
-                <input type="email" name="email" className="form-control text-center rounded-0" placeholder="Your Email"/> 
+                <input type="email" name="email" className="form-control text-center rounded-0" placeholder="Your Email" required/> 
               </div>
               <div className="mb-3 ">
-                <textarea name="message" className="form-control text-center rounded-0" rows="3" placeholder="Your Message"></textarea>
+                <textarea name="message" className="form-control text-center rounded-0" rows="3" placeholder="Your Message" required></textarea>
               </div>
               <button type="submit" className="btn btn-outline-warning btn-lg rounded-0">Submit</button>
         </form> 
